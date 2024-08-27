213b289356080a2de00a036f42d5003d13e2977c
cxl2020MC <andamu2019@163.com>
2024-08-27 20:16:21 +0800

    test

---
+++ src/types/site_config.ts
@@ -0,0 +1,43 @@
+interface SiteConfig {
+    /**
+     * The name of the site.
+     */
+    site_name: string;
+
+    /**
+     * The description of the site.
+     */
+    description: string;
+
+    /**
+     * The author of the site.
+     * This is used for social media sharing.
+     */
+    author: string;
+
+    /**
+     * The URL of the site.
+     */
+    url: string;
+
+    /**
+     * The URL of the site's logo.
+     */
+    logo: string;
+
+    /**
+     * The URL of the site's favicon.
+     */
+    favicon: string;
+
+    /**
+     * The URLs of the site's cover image.
+     * This is used for social media sharing.
+     */
+    cover: string[];
+
+    /**
+     * The URL of the site's author.
+     */
+    
+} 
