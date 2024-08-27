ac102f7bc328048fd5a85869441bdf2382ab567f
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2024-08-27 03:10:52 +0000

    test

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  ssr: true,
   app: {
     head: {
       title: 'cxl2020mc`s Blog',
@@ -9,7 +10,7 @@ export default defineNuxtConfig({
         lang: 'zh-CN'
       },
       meta: [
-        // { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
       ],
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
