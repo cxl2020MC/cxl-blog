2782f89fd74761a7f6189b730291a11680924408
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2024-08-27 03:12:33 +0000

    test

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
         lang: 'zh-CN'
       },
       meta: [
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+        // { name: 'viewport', content: 'width=device-width, initial-scale=1' }
       ],
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
