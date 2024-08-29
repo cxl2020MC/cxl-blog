f30aeb3b0eb03f9be764316a43cd121d9270dfc7
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2024-08-29 06:37:37 +0000

    test

--- src/components/BlogPostCard.vue
+++ src/components/BlogPostCard.vue
@@ -2,7 +2,7 @@
     <div class="article-card">
         <div class="article-image">
             <a :href="props.article.link">
-                <NuxtImg :src="article.image" :alt="article.title" />
+                <NuxtImg :src="article.image" :alt="article.title" placeholder placeholder-class="img-loading"  />
             </a>
         </div>
         <div class="article-info">
