2ccd2a2e1ae8747340817a6caf42127baf268c3c
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2024-08-30 02:48:38 +0000

    test

--- api/api/app.py
+++ api/api/app.py
@@ -1,18 +1,1 @@
-from fastapi import Fastapi
-from fastapi.middleware.cors import CORSMiddleware
-
-from routers import main
-
-app = Fastapi()
-
-# CORS
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-# Routers
-app.include_router(main.router)
+from core import app
