530eeb9a20d17608da2bab2b9eba9aa16e71dd07
cxl2020MC <andamu2019@163.com>
2024-09-07 14:30:19 +0800

    test

---
+++ components/blog/post/BlogPostList.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="post-list">
+        <template v-if="articles">
+            <BlogPostCard v-for="article in articles.data" :key="article.id" :article="article" />
+        </template>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import type { Article } from '~/types/article';
+
+const api_url = useRuntimeConfig().public.api_url
+const req = await useFetch < {
+    data: Array < Article >
+} > (`${api_url}/postlist`)
+const articles = req.data
+
+console.log(articles.value)
+
+</script>
+
+<style scoped>
+#post-list {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 1rem;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,13 @@
 <script setup lang="ts">
-import type { Article } from '~/types/article';
-
-useHead({
-    title: "主页",
-    meta: [
-        {
-            name: "description",
-            content: "主页",
-        },
-    ],
-})
-
-const api_url = useRuntimeConfig().public.api_url
-const req = await useFetch<{
-    data: Array<Article>
-}>(`${api_url}/postlist`)
-const articles = req.data
-
-console.log(articles.value)
+import BlogPostList from '~/components/blog/post/BlogPostList.vue';
 
 </script>
 
 <template>
-    <div id="post-list">
-        <template v-if="articles">
-            <BlogPostCard v-for="article in articles.data" :key="article.id" :article="article" />
-        </template>
-    </div>
+    <BlogPostList />
     <BlogAside />
 </template>
 
 <style scoped>
-#post-list {
-    display: flex;
-    flex-direction: column;
-    padding: 1em;
-    gap: 1em;
-}
+
 </style>
