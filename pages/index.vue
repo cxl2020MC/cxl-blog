f8afe217fc09983f05cf8933196c9bb0108da454
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2024-09-07 19:37:27 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -17,10 +17,11 @@ import BlogPostList from '~/components/blog/post/BlogPostList.vue';
 }
 
 .post-list {
-    flex: 1 1 40em;
+    flex: 1 1 50em;
 }
 
 .aside {
     flex: 1 1 10em;
+    max-width: 15em;
 }
 </style>
