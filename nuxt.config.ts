5f6c98cb13728dd874a9a575a12338e2eb7c801c
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2024-09-16 08:22:32 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -37,5 +37,17 @@ export default defineNuxtConfig({
     defaultLocale: BlogConfig.language,
   },
 
+  colorMode: {
+    preference: 'system', // default value of $colorMode.preference
+    fallback: 'light', // fallback value if not system preference found
+    hid: 'nuxt-color-mode-script',
+    globalName: '__NUXT_COLOR_MODE__',
+    componentName: 'ColorScheme',
+    classPrefix: '',
+    classSuffix: '-mode',
+    storage: 'localStorage', // or 'sessionStorage' or 'cookie'
+    storageKey: 'nuxt-color-mode'
+  }
+
   modules: ["@nuxt/icon", "@nuxtjs/seo", "@nuxtjs/color-mode"],
 });
