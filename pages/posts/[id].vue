478bd3e5d84a53e6d3187799c28c09f6cc063736
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2024-09-17 12:28:53 +0800

    Update [id].vue

--- pages/posts/[id].vue
+++ pages/posts/[id].vue
@@ -1,13 +1,38 @@
 <script setup lang="ts">
-const route
- = useRoute
-()
+const route = useRoute()
 
 // When accessing /posts/1, route.params.id will be 1
-console
-.log
-(route
-.params
-.id
-)
+console.log(route.params.id)
 </script>
+
+<template>
+    <div class="main">
+        <BlogPostList />
+        <BlogAside />
+    </div>
+</template>
+
+<style scoped>
+.main {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+
+.post-list {
+    flex: 1 1 40em;
+}
+
+.aside {
+    flex: 1 1 10em;
+}
+    
+@media (min-width: 50em) {
+    .post-list{
+        max-width: 75em;
+    }
+    .aside {
+        max-width: 20em;
+    }
+}
+</style>
