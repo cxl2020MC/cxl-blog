b04233df723af794fac7325b11c59c6b81dc53e7
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2024-09-17 06:39:11 +0000

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -50,36 +50,13 @@ export default defineNuxtConfig({
     storageKey: "nuxt-color-mode",
   },
   mdc: {
-    remarkPlugins: {
-      plugins: {
-        // Register/Configure remark plugin to extend the parser
-      },
-    },
-    rehypePlugins: {
-      options: {
-        // Configure rehype options to extend the parser
-      },
-      plugins: {
-        // Register/Configure rehype plugin to extend the parser
-      },
-    },
-    headings: {
-      anchorLinks: {
-        // Enable/Disable heading anchor links. { h1: true, h2: false }
-      },
-    },
     highlight: {
+      highlighter: "shiki", // "prism" or "shiki"
+
       langs: ['bat', 'c', 'cpp', 'css', 'diff', 'html', 'ini', 'js', 'json', 'log', 'makefile', 'matlab', 'md', 'mdc', 'powershell', 'python', 'shell', 'ssh-config', 'toml', 'ts', 'tsx', 'vue', 'xml', 'yaml'],
       theme: {
         default: 'catppuccin-latte',
         dark: 'one-dark-pro',
-      },
-    },
-    // Control syntax highlighting
-    components: {
-      prose: false, // Add predefined map to render Prose Components instead of HTML tags, like p, ul, code
-      map: {
-        // This map will be used in `<MDCRenderer>` to control rendered components
       },
     },
   },
